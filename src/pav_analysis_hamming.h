46bd9bfbc5c36c9662bc5e2ead4fc3c01d4e6a43
jorgeMarin1 <100561030+jorgeMarin1@users.noreply.github.com>
2022-03-05 22:06:17 +0100

    Update pav_analysis_hamming.h
    se ha quitado el argumento M y trm de la función que calcula de la potencia

--- src/pav_analysis_hamming.h
+++ src/pav_analysis_hamming.h
@@ -3,7 +3,7 @@
 
 
 float hamming(float *w, unsigned int N);
-float compute_power(const float *x, const float *w, unsigned int N, unsigned int M, int trm, float ew);
+float compute_power(const float *x, const float *w, unsigned int N, float ew);
 float compute_am(const float *x, unsigned int N);
 float compute_zcr(const float *x, unsigned int N, float fm);
 
